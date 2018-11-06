4aabbda6414a9b1b3718838af49fa59e98c3d922
Egidio Ricchiuti <egisan3@yahoo.com>
2018-11-06 14:48:10 +0100

    Första ändringen action metod och länk

--- HelloAgainWeb/Controllers/HomeController.cs
+++ HelloAgainWeb/Controllers/HomeController.cs
@@ -39,6 +39,12 @@ namespace HelloAgainWeb.Controllers
             return View();
         }
 
+        public IActionResult EgidioJens()
+        {
+
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
