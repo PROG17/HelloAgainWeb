e082346b8e9f9ac7d12609222a410f7b65d923cf
David A <mercenary_88@hotmail.com>
2018-11-06 14:41:52 +0100

    Davids ssida

--- HelloAgainWeb/Controllers/HomeController.cs
+++ HelloAgainWeb/Controllers/HomeController.cs
@@ -39,6 +39,11 @@ namespace HelloAgainWeb.Controllers
             return View();
         }
 
+        public IActionResult DavidAgdelius()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
