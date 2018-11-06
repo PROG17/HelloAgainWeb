9a15780c917bc8acb0ac884c5d931139aab9e9c4
Fredrik Haglund <mejl@fredrikhaglund.se>
2018-11-06 10:00:54 +0100

    Add project files.

---
+++ HelloAgainWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloAgainWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
