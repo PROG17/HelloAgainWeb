f62f64881b4d21b5c582d65b36399cdbffa94950
Elias Mohammed <eliasrmohammed@hotmail.com>
2018-11-06 14:43:25 +0100

    Minsida

--- HelloAgainWeb/Controllers/HomeController.cs
+++ HelloAgainWeb/Controllers/HomeController.cs
@@ -38,6 +38,10 @@ namespace HelloAgainWeb.Controllers
         {
             return View();
         }
+        public IActionResult EliasMohammed()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
